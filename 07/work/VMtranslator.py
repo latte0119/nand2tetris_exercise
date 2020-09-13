c3b1e2f0ca03d425a9ad6da32435ec4d0945771e
latte0119 <latte.malta@gmail.com>
2020-09-13 19:06:02 +0900

    tmp

--- 07/work/VMtranslator.py
+++ 07/work/VMtranslator.py
@@ -1,15 +1,78 @@
 import os,sys
 import re
-
+from enum import Enum
 def is_vm_file(filename):
-    return True if os.path.isfile(filename) and re.fullmatch(r'*.vm',filename) else False
+    return True if os.path.isfile(filename) and re.fullmatch(r'.*vm',filename) else False
+
+class CommandType(Enum):
+    C_ARITHMETIC=0
+    C_PUSH=1
+    C_POP=2
+    C_LABEL=3
+    C_GOTO=4
+    C_IF=5
+    C_FUNCTION=6
+    C_RETURN=7
+    C_CALL=8
+
 
 class Parser:
-    def __init__(self,filename):
-        with open(filename) as f:
+    def __init__(self,filepath):
+        self.filename=os.path.splitext(os.path.basename(filepath))[0]
+        with open(filepath) as f:
             self.lines=f.readlines()                
 
+
+class CodeWriter:
+    def __init__(self,filepath): #書き込み先
+        self.ost=open(filepath,mode='w')
+    
+    def setFileName(self,filename): #読み込むファイル名を設定
+        self.filename=filename
+    
+    def writeArithmetic(self,command):
+        pass
+    
+    def writePushPop(self,command,segment,index):
+        mp={
+            'local':'LCL',
+            'argument':'ARG',
+            'this':'THIS',
+            'that':'THAT',
+        }
+
+        if command==CommandType.C_PUSH:
+            if segment in mp:
+                self.ost.write('@',mp[segment],sep='')
+                self.ost.write('D=A')
+                self.ost.write('@',index,sep='')
+                self.ost.write('A=D+A')
+                self.ost.write('D=M')
+            if segment=='constant':
+                self.ost.write('@',index,sep='')
+                self.ost.write('D=A')
+            if segment=='static':
+                self.ost.write('@',self.filename,'.',index,sep='')
+                self.ost.write('D=M')
+            self.ost.write('@SP')
+            self.ost.write('M=D')
+            self.ost.write('M=M+1')
         
+        else:
+            self.ost.write('@SP')
+            self.ost.write('M=M-1')
+            self.ost.write('D=M')
+            if segment in mp:
+                self.ost.write('@',mp[segment],sep='')
+                self.ost.write('D=A')
+                self.ost.write('@',index,sep='')
+                self.ost.write('A=D+A')
+            if segment=='static':
+                self.ost.write('@',self.filename,'.',index,sep='')
+            self.ost.write('')
+    def __del__(self):
+        self.ost.close()
+
 if __name__=='__main__':
     assert len(sys.argv)>1,'error : incorrect number of arguments'
 
@@ -18,7 +81,11 @@ if __name__=='__main__':
     if os.path.isfile(path):
         filelist=[path]
     else:
-        filelist=os.listdir(path)
-
-    programs=list(map(lambda filename :Parser(filename),filter(is_vm_file,filelist)))
+        filelist=[os.path.join(path,filename) for filename in os.listdir(path)]
     
+    programs=list(map(lambda filepath :Parser(filepath),filter(is_vm_file,filelist)))
+    print(len(programs))
+    for p in programs:
+        print(p.filename)
+    
+    codewriter=CodeWriter('tmp.hack')
